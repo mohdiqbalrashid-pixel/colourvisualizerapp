e5116817591c86ad594e3eee876d5a2fad905945
mohdiqbalrashid-pixel <mohd.iqbal.rashid@gmail.com>
2026-07-08 11:42:43 +0400

    Update session.py

--- modules/session.py
+++ modules/session.py
@@ -1,1 +1,20 @@
+import streamlit as st
 
+def initialise_session() -> None:
+    """Sets up the blank memory states for the application on first load."""
+    defaults = {
+        "uploaded_image": None,
+        "selected_colour": None,
+        "selected_surface_point": None,
+        "wall_mask": None,
+        "painted_image": None,
+        "paint_strength": 1.0,
+    }
+    
+    for key, value in defaults.items():
+        if key not in st.session_state:
+            st.session_state[key] = value
+
+def sync_legacy_to_app() -> None:
+    """A placeholder to keep app.py happy if we need to sync old variables later."""
+    pass
